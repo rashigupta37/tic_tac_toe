52402d01c6b574af725d912a2f9457268c4ceb9f
rashigupta37 <43826578+rashigupta37@users.noreply.github.com>
2020-07-24 23:45:16 +0530

    Update playerRandom.py

--- playerRandom.py
+++ playerRandom.py
@@ -1,8 +1,9 @@
 from random import choice, seed
-
+#when player chooses to play against random player 
 def playerRandom(game):
-    
+    #to initialize the random number generator
     seed()
+    #choice() is used to returns a random item from a list
     myMove = choice(game.getEmp())
     
     return myMove
